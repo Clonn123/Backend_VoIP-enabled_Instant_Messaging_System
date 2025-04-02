4e6ebdea2912629923d86ad25d650db0806bb5a7
Poloznikov Artem <art-clon@mail.ru>
2025-04-02 21:34:13 +0500

    Авторизация есть

--- auth-service/app/routes/auth.py
+++ auth-service/app/routes/auth.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from ..database import supabase
 from ..schemas import UserRegister, UserLogin
+from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 router = APIRouter(prefix="/auth")
+security = HTTPBearer()
 
 @router.post("/register")
 async def register(user: UserRegister):
@@ -45,3 +48,45 @@ async def login(user: UserLogin):
         }
     except Exception as e:
         raise HTTPException(status_code=401, detail="Invalid credentials")
+
+# Пока опустим, так как выход по кнопке на фронте, но можно и тут добавить будет
+# @router.post("/logout")
+# async def logout(credentials: HTTPAuthorizationCredentials = Depends(security)):
+#     try:
+#         # Извлекаем токен из HTTPBearer
+#         token = credentials.credentials
+        
+#         # Проверяем, что токен не пустой
+#         if not token:
+#             raise HTTPException(status_code=401, detail="Token is missing")
+        
+#         # Выход из системы через Supabase
+#         supabase.auth.sign_out(token)
+#         return {"status": "success"}
+    
+#     except Exception as e:
+#         raise HTTPException(
+#             status_code=400,
+#             detail=f"Logout failed: {str(e)}"
+#         )
+
+async def get_current_user(request: Request):
+    # Извлекаем токен из заголовка
+    token = request.headers.get("Authorization", "").replace("Bearer ", "")
+    
+    try:
+        # Проверяем токен через Supabase
+        user = supabase.auth.get_user(token)
+        return user
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+@router.get("/me")
+async def get_profile(user = Depends(get_current_user)):
+    profile = supabase.table("profiles") \
+        .select("*") \
+        .eq("user_id", user.user.id) \
+        .single() \
+        .execute()
+    
+    return profile.data
